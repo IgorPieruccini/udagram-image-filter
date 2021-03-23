8238325f6d77132442fa843d0b942917a93600b1
igor.pieruccini <igor.pieruccini@wooga.net>
2021-03-23 10:25:59 +0100

    add imageResponse type

--- src/util/util.ts
+++ src/util/util.ts
@@ -39,7 +39,7 @@ interface isImageResponse {
   isSupportedFormat: boolean;
   encrypted: boolean;
 }
-const isImage = (url: string) => {
+const isImage = (url: string): isImageResponse => {
   const isSupported = supportedFormat.some((val) => url.includes(val));
   return {
     isSupportedFormat: isSupported,
